0488d28c4254db3c8215205832fe14f51c6fb29c
Simon Lake <simonl@theodo.co.uk>
2023-03-28 17:43:10 +0100

    feat: animated text fill

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text outline">Design Guild Demo 5</div>
+      <div className="text fill">Design Guild Demo 5</div>
+    </div>
+  );
+}
+
+export default App;
